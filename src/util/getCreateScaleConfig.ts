b22598a216fb964cf82c601a6d0073fe2a7cdcb6
Jesse Stuart <email@jessestuart.ca>
2020-08-08 13:07:50 -0400

    fix getCreateScaleConfig to ensure ScaleRequest always has one
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- src/util/getCreateScaleConfig.ts
+++ src/util/getCreateScaleConfig.ts
@@ -1,9 +1,10 @@
 import { ScaleConfig, CreateScaleRequest } from '../generated/controller_pb';
 
 export default function getCreateScaleConfig(req: CreateScaleRequest): ScaleConfig {
-	const config = req.getConfig();
-	if (config) {
-		return config;
+	let config = req.getConfig();
+	if (!config) {
+		config = new ScaleConfig();
+		req.setConfig(config);
 	}
-	return new ScaleConfig();
+	return config;
 }
